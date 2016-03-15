4fd04fd3f1e90bc056ee1179eb4cf825f5d11d03
Fredrik Julin <fjn14001@student.mdh.se>
2016-03-16 00:30:55 +0100

    Main instruktion, hur vi kallar på InitOgL. Samt hur vi skapar ett window.

--- DVA222_Project/DVA222_Project.cpp
+++ DVA222_Project/DVA222_Project.cpp
@@ -18,10 +18,18 @@ int _tmain(int argc, char** argv)
 	//From that point the control is handed over to the OpenGL window and stays there until the user closes the Window.
 	//This means after calling the InitOGL function no further lines of code in the main function are executed until we close the Window.
 	//------------------------------------------------------------------------------------------------------------------------------------
-	ZControlBase** Controls = new ZControlBase*[2];
-	Controls[0] = new Button(20, 20, 100, 50, 1);
-	Controls[1] = new ImageBox(50,50, 150, 60, 2);
-	InitOGL(argc, argv, *Controls);
+
+
+
+	/*EXEMPEL PÅ INITIERING OCH TILLÄG AV SAKER --->>
+	RadioButtonGroup * radioGroup = new RadioButtonGroup("titel", storlek)
+	radiogroup->add(new Radiobutton);
+	window->add(radioGroup);*/
+
+	MyButton *button = new MyButton(20, 40, 150, 200);
+	InitOGL(argc, argv, button);
+
+
 
 	return 0;
 }
--- DVA222_Project/Window.h
+++ DVA222_Project/Window.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Container.h"
 #include "Label.h"
+#include <vector>
 class Window : public Container , public Label
 {
 public:
+	//ZControlbase array fylld med objekt. Initierar sedan ett window i main och skickar in det objektet i InitOGL
+	vector <ZControlBase> Objects;
 	Window(int x, int y, int width, int height, int z);
+
+	void Add();
+
 	~Window();
 };
-
