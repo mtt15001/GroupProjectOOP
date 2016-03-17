8fc39c719dc454d133fa30f5725998243fdf6f98
Martin Bergman Törnkvist <mtt15001@student.mdh.se>
2016-03-17 09:42:00 +0100

    inget

--- DVA222_Project/RadioButtonGroup.h
+++ DVA222_Project/RadioButtonGroup.h
@@ -6,10 +6,8 @@ class RadioButtonGroup : public Container
 public:
 	RadioButtonGroup(int x, int y, int width, int height, int z, string n);
 	void AddObject(ZControlBase *Obj);
-
 	~RadioButtonGroup();
 private:
 	Label title;
 	
 };
-
